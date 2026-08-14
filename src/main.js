502bb4af82d1c1d71e722f8d9d14606dcbe8170c
Bunnasorn Kaewsiri <bunnasorn.k@outlook.com>
2026-08-15 00:18:40 +0700

    Add license

--- src/main.js
+++ src/main.js
@@ -6,6 +6,8 @@ import App from "./App.vue";
 
 createApp(App)
   .use(PrimeVue, {
+    license:
+      "eyJpZCI6IjkzOGJhMDE1LWFlNDYtNDNjOC1hMTIzLWQ3ZTBiODBlYTVjNSIsInByb2R1Y3QiOiJwcmltZXVpIiwidGllciI6ImNvbW11bml0eSIsInR5cGUiOiJkZXYiLCJpYXQiOjE3ODY3Mjc2ODgsImV4cCI6MTgxODI2MzY4OH0.t2mnhiIr0yehlJDnEMqZs5bll_4BsFFrVkW1xHG8qfjcecL09pgl7Y48rXo3-i7ND6swPMxriF37oA5oh5WrDQ",
     theme: {
       preset: Aura,
       options: {
